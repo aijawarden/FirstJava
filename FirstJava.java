56b8bfaadc0d6779ce850d39269aadeb082954f2
aijawarden <76536027+aijawarden@users.noreply.github.com>
2020-12-23 12:18:54 -0800

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "Hello from Java" );
+    System.out.println( "This is the second line of output" );
+  }
+} 
